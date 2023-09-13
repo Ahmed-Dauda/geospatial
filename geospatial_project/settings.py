055ce95078338ba844459ea89afa0e93cfee64d8
Ahmed-Dauda <addatainsight@gmail.com>
2023-09-13 13:32:39 -0700

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- geospatial_project/settings.py
+++ geospatial_project/settings.py
@@ -123,15 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
 # settings.py
 
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
-# Static root
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
+
 
 
 # Default primary key field type
